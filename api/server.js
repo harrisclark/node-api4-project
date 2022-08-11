1a603b684d03d0183e30fd6d4c6465c70ba493af
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-08-11 18:31:08 -0500

    ready to test on heroku

--- api/server.js
+++ api/server.js
@@ -3,6 +3,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+  res.send([])
+});
 
+server.post('/api/register', (req, res) => {
+  //console.log(req.body)
+  res.send(req.body.username)
+});
+
+server.post('/api/login', (req, res) => {
+  const message = `Well, hello there, ${req.body.username}! Welcome to the internet!`
+  res.send(message);
+})
 
 module.exports = server;
