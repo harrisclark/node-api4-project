b59d73d7de7f63a1d30e8a20fdfa3538d57e75cc
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-08-11 18:53:45 -0500

    make fakeUsers array

--- api/server.js
+++ api/server.js
@@ -3,16 +3,22 @@ const server = express();
 
 server.use(express.json());
 
+const fakeUsers = [
+  { username: "mario", password: "it's-a-me" },
+  { username: "gandalf", password: "grayPilgrim" },
+]
+
 server.get('/', (req, res) => {
   res.send('I sure hope this is what we were supposed to deploy')
 })
 
 server.get('/api/users', (req, res) => {
-  res.send([])
+  res.send(fakeUsers)
 });
 
 server.post('/api/register', (req, res) => {
   //console.log(req.body)
+  fakeUsers.push(req.body)
   res.send(req.body.username)
 });
 
